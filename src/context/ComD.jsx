bbc93eb37514efafe81324ee14414156668b7ba1
B-nod <binodtamang245@gmail.com>
2024-06-08 22:05:00 +0545

    show'

---
+++ src/context/ComD.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ComD = () => {
+  return (
+    <>
+    <h1>The programming langauge we are learning is Javascript</h1>
+    
+    </>
+  )
+}
+
+export default ComD
