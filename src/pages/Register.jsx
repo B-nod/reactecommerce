42ac8f427d28ed84458d942d6fac5e40c3b5fe7b
B-nod <binodtamang245@gmail.com>
2024-06-08 21:57:59 +0545

    'formvalid'

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import { Formik,Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 
 const Register = () => {
   return (
  
-    <Formik>
-<div className="container">
+    <Formik
+    // in initalvalues we define object
+    initialValues={{firstname:'', lastname:'', email:'', password:'', cpassword:''}}
+
+    validationSchema={Yup.object({
+        firstname:Yup.string()
+        .max(20,'must be 20 characters as less')
+        .required('firstname is mandatory'),
+
+        lastname:Yup.string()
+        .max(20, 'must be 20 characgters or less')
+        .required('lastname is mandatory'),
+
+        email:Yup.string()
+        .email('invalid email format')
+        .required('email is mandatory'),
+
+        password:Yup.string()
+        .matches(/(?=.[A-Za-z])(?=.*[0-9])(?=.*[s#$-_?!])[A-Za-z0-9@#$-_?!]{8,9}$/,'must be minimum 8 character and contain uppercase, lowercase, numeric and special characters')
+        .required('password is mandatory'),
+
+        cpassword:Yup.string()
+        .required('Confirm password is mandatory')
+        .oneOf([Yup.ref('password'), null],'password and confirm password must match')
+    
+            
+    })}
+
+
+    >
+<div className="container my-5">
     <div className="row d-flex justify-content-center">
         <div className="col-md-5 shadow-lg">
             <Form>
@@ -38,6 +68,14 @@ const Register = () => {
                     <label htmlFor="password">Password</label>
                     <Field type="password" name="password" id="password" className="form-control"/>
                     <ErrorMessage name='password'>
+                        {msg=><div style={{color:'red'}}>{msg}</div>}
+                    </ErrorMessage>
+
+                </div>
+                <div className="mb-2">
+                    <label htmlFor="cpassword">Confirm Password</label>
+                    <Field type="password" name="cpassword" id="cpassword" className="form-control"/>
+                    <ErrorMessage name='cpassword'>
                         {msg=><div style={{color:'red'}}>{msg}</div>}
                     </ErrorMessage>
 
